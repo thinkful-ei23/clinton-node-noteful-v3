882867bdfdf2ab32440ce47009543a51a683e08b
Clinton Owen <clint@clintonowen.com>
2018-08-15 16:11:26 -0500

    completed basic CRUD tests in notes.test.js

--- test/notes.test.js
+++ test/notes.test.js
@@ -105,4 +105,95 @@ describe('Noteful API resource', function() {
 
   });
 
+  describe('POST /api/notes', function() {
+
+    it('should create and return a new note when provided valid data', function() {
+      const newItem = {
+        'title': 'The best article about dogs ever!',
+        'content': 'Lorem ipsum dolor...'
+      };
+
+      let res;
+      return chai.request(app)
+        .post('/api/notes')
+        .send(newItem)
+        .then(function(_res) {
+          res = _res;
+          expect(res).to.have.status(201);
+          expect(res).to.have.header('location');
+          expect(res).to.be.json;
+
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
+
+          return Note.findById(res.body.id);
+        })
+        .then(function(data) {
+          expect(res.body.id).to.equal(data.id);
+          expect(res.body.title).to.equal(data.title);
+          expect(res.body.content).to.equal(data.content);
+          expect(new Date(res.body.createdAt)).to.eql(data.createdAt);
+          expect(new Date(res.body.updatedAt)).to.eql(data.updatedAt);
+        });
+    });
+
+  });
+
+  describe('PUT /api/notes/:id', function() {
+
+    it('should update the note when provided valid data', function() {
+      const updateData = {
+        title: 'Updated Title',
+        content: 'Updated content lorem ipsum...'
+      };
+
+      return Note
+        .findOne()
+        .then(function(note) {
+          updateData.id = note.id;
+
+          return chai.request(app)
+            .put(`/api/notes/${note.id}`)
+            .send(updateData);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
+
+          return Note.findById(updateData.id);
+        })
+        .then(function(note) {
+          expect(note.title).to.equal(updateData.title);
+          expect(note.content).to.equal(updateData.content);
+        });
+
+    });
+
+  });
+
+  describe('DELETE /api/notes/:id', function() {
+
+    it('should delete a note by id', function() {
+      let note;
+
+      return Note
+        .findOne()
+        .then(function(_note) {
+          note = _note;
+          return chai.request(app).delete(`/api/notes/${note.id}`);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(204);
+          return Note.findById(note.id);
+        })
+        .then(function(_note) {
+          expect(_note).to.be.null;
+        });
+    });
+
+  });
+
 });

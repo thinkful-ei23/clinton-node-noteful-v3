e4496788beaeaded43192a2e8f660ccf8f17487e
Clinton Owen <clint@clintonowen.com>
2018-08-17 14:32:18 -0500

    added GET tag by id tests

--- test/tags.test.js
+++ test/tags.test.js
@@ -80,4 +80,51 @@ describe('Noteful /api/tags resource', function() {
 
   });
 
+  describe('GET /api/tags/:id', function() {
+
+    it('should return the correct tag', function() {
+      let resTag;
+      return Tag.findOne()
+        .then(function(res) {
+          resTag = res;
+          return chai.request(app).get(`/api/tags/${resTag.id}`);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.keys('id', 'name', 'createdAt', 'updatedAt');
+          expect(res.body.id).to.equal(resTag.id);
+          expect(res.body.name).to.equal(resTag.name);
+          expect(new Date(res.body.createdAt)).to.eql(resTag.createdAt);
+          expect(new Date(res.body.updatedAt)).to.eql(resTag.updatedAt);
+        });
+    });
+
+    it('should return a 400 error when given an invalid id', function() {
+      return chai.request(app)
+        .get('/api/tags/NOTANID')
+        .then(function(res) {
+          expect(res).to.have.status(400);
+          expect(res).to.be.json;
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.include.keys('message', 'status');
+          expect(res.body.message).to.equal('Invalid id');
+        });
+    });
+
+    it('should return a 404 error when given a nonexistent id', function() {
+      return chai.request(app)
+        .get('/api/tags/222222222222222222222299')
+        .then(function(res) {
+          expect(res).to.have.status(404);
+          expect(res).to.be.json;
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.include.keys('message', 'status');
+          expect(res.body.message).to.equal('Not Found');
+        });
+    });
+
+  });
+
 });

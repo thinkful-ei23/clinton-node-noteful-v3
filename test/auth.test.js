c952c42ad7b4e7fb69e614b205f560d6084b8395
Clinton Owen <clint@clintonowen.com>
2018-08-23 10:59:59 -0500

    completed first two login tests

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,23 +1,69 @@
 'use strict';
 
+const app = require('../server');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+
+const { TEST_MONGODB_URI } = require('../config');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe.only('Noteful API - Auth', function () {
-  before();
-  beforeEach();
-  afterEach();
-  after();
+  const username = 'exampleUser';
+  const password = 'examplePass';
+  const fullname = 'Example User';
+
+  before(function() {
+    return mongoose.connect(TEST_MONGODB_URI)
+      .then(() => mongoose.connection.db.dropDatabase());
+  });
+
+  beforeEach(function() {
+    const exampleUser = { username, password, fullname };
+    return chai.request(app)
+      .post('/api/users')
+      .send(exampleUser);
+  });
+
+  afterEach(function() {
+    return mongoose.connection.db.dropDatabase();
+  });
+
+  after(function() {
+    return mongoose.disconnect();
+  });
 
   describe('/api/login', function() {
 
-    it('should reject requests with no credentials');
+    it('should reject requests with no credentials', function() {
+      return chai.request(app)
+        .post('/api/login')
+        .then((res) => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.be.an('object');
+          expect(res.body.name).to.equal('AuthenticationError');
+          expect(res.body.message).to.equal('Bad Request');
+          expect(res.body.status).to.equal(400);
+        });
+    });
 
-    it('should reject requests with incorrect usernames');
+    it('should reject requests with incorrect usernames', function() {
+      const badUser = { username: 'joeschmoe', password };
+
+      return chai.request(app)
+        .post('/api/login')
+        .send(badUser)
+        .then(res => {
+          expect(res).to.have.status(401);
+          expect(res.body).to.be.an('object');
+          expect(res.body.name).to.equal('AuthenticationError');
+          expect(res.body.message).to.equal('Unauthorized');
+          expect(res.body.status).to.equal(401);
+        });
+    });
 
     it('should reject requests with incorrect passwords');
 
